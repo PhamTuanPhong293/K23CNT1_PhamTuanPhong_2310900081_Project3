5a3be8cefa1356c863c771ffa1c1a5e7b0dc692b
PhamTuanPhong293 <phongpham29305@gmail.com>
2025-10-20 22:15:28 +0700

    update

---
+++ Lession01-spingboot/src/main/java/com/ptphong/Lession01_spingboot/Lession01SpingbootApplication.java
@@ -0,0 +1,14 @@
+package com.ptphong.Lession01_spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lession01SpingbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lession01SpingbootApplication.class, args);
+        System.out.println("Hello World!");
+	}
+
+}
