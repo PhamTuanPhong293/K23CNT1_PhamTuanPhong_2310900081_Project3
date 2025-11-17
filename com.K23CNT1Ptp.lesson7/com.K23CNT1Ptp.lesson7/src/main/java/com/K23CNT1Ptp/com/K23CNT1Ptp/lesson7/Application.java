b65fb0896d61fbbfd212f48d26e872dcc6435936
PhamTuanPhong293 <phongpham29305@gmail.com>
2025-11-17 08:09:37 +0700

    update

---
+++ com.K23CNT1Ptp.lesson7/com.K23CNT1Ptp.lesson7/src/main/java/com/K23CNT1Ptp/com/K23CNT1Ptp/lesson7/Application.java
@@ -0,0 +1,13 @@
+package com.K23CNT1Ptp.com.K23CNT1Ptp.lesson7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ com.K23CNT1Ptp.lesson7/com.K23CNT1Ptp.lesson7/src/test/java/com/K23CNT1Ptp/com/K23CNT1Ptp/lesson7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.K23CNT1Ptp.com.K23CNT1Ptp.lesson7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
